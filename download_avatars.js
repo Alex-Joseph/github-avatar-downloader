ba2fc22554a15fd2a0ea03daf18d12d1a4e8834e
Alex-Joseph <alex.cheng@ryerson.ca>
2017-02-13 23:03:24 +0000

    request successful

--- download_avatars.js
+++ download_avatars.js
@@ -6,9 +6,14 @@ var GITHUB_TOKEN = "551188a057e2f5497e24d338e892f0aa59786da0";
 
 function  getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {url : requestURL, headers: {'User-Agent': "GitHub Avatar Downloader - Student Project"} };
+  request.get(options, function(err, response, body) {
+    if (err) throw err;
+    console.log("Response Status Code:", response.statusCode);
+    console.log(body);
+  });
 }
-
+// calls the function
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("result:", result);
