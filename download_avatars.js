171d33b4e27ad4660965f01f529ac9c0daa33736
Alex-Joseph <alex.cheng@ryerson.ca>
2017-02-13 16:39:46 +0000

    creates requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+var GITHUB_USER = "Alex-Joseph";
+var GITHUB_TOKEN = "551188a057e2f5497e24d338e892f0aa59786da0";
+
+function  getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("result:", result);
+});
